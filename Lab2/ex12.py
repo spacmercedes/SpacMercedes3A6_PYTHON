b27d0a30517cf1a800b3eb2ebcbbd19f9bdfe4f5
spacmercedes <79169837+spacmercedes@users.noreply.github.com>
2022-10-17 12:59:38 +0300

    ex 6 s 12

--- Lab2/ex12.py
+++ Lab2/ex12.py
@@ -1,18 +1,12 @@
-def ex1(n):
+from typing import List
 
-    q = []
-    for i in range(n):
-        if(i==0):
-            q.append(0)
-        if(i==1):
-            q.append(1)
-        else:
-            q.append(q[i]+q[i-1])
-    q.pop(0)
-    return q
-    
-    
 
+def ex12(input_words: List[str]) -> List[List[str]]:
+   
+    return [t for t in
+            [[input_words[x] for x in range(y, len(input_words)) if input_words[x][-2:] == input_words[y][-2:] and
+              input_words[x][-2:] not in [input_words[w][-2:] for w in range(y)]] for y in range(len(input_words))]
+            if len(t) > 0]
 
 if __name__ == '__main__':
-    print(ex1(6))
+    print(ex12(['ana', 'banana', 'carte', 'arme', 'parte']))
--- Lab2/ex6.py
+++ Lab2/ex6.py
@@ -1,25 +1,12 @@
-# def ex6(*lists, count):
-
-#     all_lists = []
-#     return_list = []
-#     for lst in lists:
-#         all_lists+=lst
-#     for item in all_lists:
-#         if all_lists.count(item) == count and item not in return_list:
-#             return_list.append(item)
-
-#     return return_list
+from typing import List
 
 
-# if __name__ == '__main__':
-#     print([1,2,3], [2,3,4],[4,5,6], 3)
-
-def elements(x, *lists):
-    newList = []
-    for l in lists:
-        newList += l
-    return [e for e in set(newList) if newList.count(e) == x]
-
+def ex6(*lists: List, x: int) -> List:
+  
+    united_lists = []
+    for i in lists:
+        united_lists += i
+    return list(set([i for i in united_lists if united_lists.count(i) == x]))
 
 if __name__ == '__main__':
-    print(elements(2, [1, 2, 3], [2, 3, 4], [4, 5, 6], [4, 1, "test"]))
+    print(ex6([1, 2, 3], [1, 2, 3], [4, 4, 4], [5, 5, 5, 5], x=2))
